d54f86aef7fd9012617a09ad7d94354b9edb2d8f
deeprock2 <38926443+deeprock2@users.noreply.github.com>
2022-01-23 04:02:25 +0300

    Update gpu-driver-manager.sh

--- gpu-driver-manager.sh
+++ gpu-driver-manager.sh
@@ -50,48 +50,6 @@ if [[ $(whoami) == "root" ]]; then
 			echo "/usr/lib/modprobe.d/nvidia-utils.conf dosyasını incele çünkü beklediğimden farklı içeriği var."
 		fi
 	fi
-
-	file2="/usr/lib/modprobe.d/nvidia-lts.conf"
-
-	if [ -s "$file2" ];then 
-	   nvidiaBlackListDurumu=`cat /usr/lib/modprobe.d/nvidia-lts.conf | grep "blacklist nouveau"`
-	   if [[ $nvidiaBlackListDurumu == "blacklist nouveau" ]]; then
-			echo "Şuan; nouveau (açık kaynak sürücü), $file2 dosyasındaki blacklist'tedir."
-			echo "Şuan, nvidia-lts (kapalı kaynak sürücü) aktiftir."
-			echo -n "nvidia açık kaynak sürücüsünü (nouveau / xf86-video-nouveau) aktif etmek ister misin? [E/h] "
-			read responseForNouveou
-			if [[ "$responseForNouveou" == "e" || "$responseForNouveou" == "E" || "$responseForNouveou" == "evet" ]]; then
-					rm $file2
-					touch $file2
-					echo "#blacklist nouveau" > $file2
-					echo "İlgili yapılandırma dosyası düzenlendi."
-					echo "nouveau açık kaynak sürücüsünün aktif olması için reboot yapmak gerekiyor."
-					echo -n "Şimdi reboot yapılsın mı? [E/h] "
-					read responseForReboot
-					if [[ "$responseForReboot" == "e" || "$responseForReboot" == "E" || "$responseForReboot" == "evet" ]]; then
-						reboot
-					fi
-				fi
-		elif [[ $nvidiaBlackListDurumu == "#blacklist nouveau" ]]; then
-			echo "Şuan; nouveau, $file2 dosyasındaki blacklist'te değildir."
-				echo -n "nvidia-lts (kapalı kaynak) sürücüsünü aktif etmek ister misin? [E/h] "
-				read response
-				if [[ "$response" == "e" || "$response" == "E" || "$response" == "evet" ]]; then
-					rm $file2
-					touch $file2
-					echo "blacklist nouveau" > $file2
-					echo "İlgili yapılandırma dosyası düzenlendi."
-					echo "nvidia-lts kapalı kaynak sürücüsünün aktif olması için reboot yapmak gerekiyor."
-					echo -n "Şimdi reboot yapılsın mı? [E/h] "
-					read responseForReboot
-					if [[ "$responseForReboot" == "e" || "$responseForReboot" == "E" || "$responseForReboot" == "evet" ]]; then
-						reboot
-					fi
-				fi
-		else
-			echo "/usr/lib/modprobe.d/nvidia-lts.conf dosyasını incele çünkü beklediğimden farklı içeriği var."
-		fi
-	fi
 else
 	echo "Lütfen, superuser (root) olarak programı çalıştırınız. "
 fi
